fe70d72dee9de804541473e99e36f91c254111d2
putriyasshiro <putriyasshiro99@gmail.com>
2019-10-11 10:41:55 +0700

    tugas di kumpul di kelas

---
+++ week6/Contoh1.php
@@ -0,0 +1,5 @@
+<?php
+	$a = 2;
+	$hasil = ($a<4) ? ($a*2) : ($a*3);
+	echo "hasilnya adalah $hasil";
+?>
---
+++ week6/Latihan 1.1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Contoh Skrip PHP </title>
+    </head>
+    <body>
+        <?php
+        $nama = "Kim Taehyung";
+        echo "$nama berkata \"Save me save me\" ";
+        ?>
+    </body>
+</html>
